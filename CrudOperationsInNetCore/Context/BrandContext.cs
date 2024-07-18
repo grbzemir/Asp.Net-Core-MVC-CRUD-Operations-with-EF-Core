43143b0254df93b3af8c2d32c589a93232e4fac2
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-07-18 13:11:59 +0300

    Add project files.

---
+++ CrudOperationsInNetCore/Context/BrandContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudOperationsInNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInNetCore.Context
+{
+    public class BrandContext : DbContext
+    {
+
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+
+
+    }
+}
---
+++ CrudOperationsInNetCore/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿namespace CrudOperationsInNetCore.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? SurName { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
